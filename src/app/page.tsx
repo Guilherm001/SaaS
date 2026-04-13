ec84ba65f8cc4e10b7d297500953a6def6e27949
Guilherme <G.Asolutions@outlook.com>
2026-04-13 16:04:53 -0300

    primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>ola planeta terra</h1>
+      <Button variant="destructive">Button</Button>
+    </div>
+  )
+}
